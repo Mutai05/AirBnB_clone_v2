26ebca9f29c6382582b96bd052b178e787bf365b
Mutai05 <kelvok2014@gmail.com>
2024-03-07 15:07:14 -0800

    Updated code format style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from fabric.api import local, lcd
+from fabric.api import local
 from time import strftime
+from datetime import date
+
 
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
@@ -8,8 +10,8 @@ def do_pack():
     filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        with lcd("web_static"):
-            local("tar -czvf ../versions/web_static_{}.tgz *".format(filename))
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
         return "versions/web_static_{}.tgz".format(filename)
 
